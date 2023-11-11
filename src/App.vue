30f62a055261927de9aa0de95e463f662ecec27c
jagger-en <japman2400@gmail.com>
2023-11-11 16:34:00 +0200

    Add and delete notifications

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@ import Notification from './components/Notification.vue'
 
 import firebaseConfig from "../firebaseInit";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs,
+         addDoc, deleteDoc, doc } from "firebase/firestore";
 </script>
 
 <script>
+const app = initializeApp(firebaseConfig);
+const fs = getFirestore(app)
+const collection_ref = collection(fs, "notifications")
 export default {
   data() {
     return {
@@ -20,18 +24,24 @@ export default {
   },
   methods: {
     async getNotifications() {
-      const app = initializeApp(firebaseConfig);
-      const fs = getFirestore(app)
-      const collection_ref = collection(fs, "notifications")
 
       const notifications = []
       await getDocs(collection_ref).then((snapshot) => {
         snapshot.docs.forEach(d => {
-          notifications.push(d.data())
+          notifications.push({id: d.id, ...d.data()})
         })
       })
 
       return notifications
+    },
+    async addNotification() {
+      await addDoc(collection_ref, {'msg': 'foo-bar'})
+      window.location.reload()
+    },
+    async deleteNotification(notification) {
+      const docRef = doc(fs, 'notifications', notification.id)
+      await deleteDoc(docRef)
+      window.location.reload()
     }
   }
 }
@@ -41,8 +51,9 @@ export default {
   <main>
     <h1>Hello World</h1>
     <li v-for="notification in notifications">
-      {{ notification.msg }}
+      {{ notification.msg }} <button @click="deleteNotification(notification)">Del</button>
     </li>
     <Notification msg="This is the notification." />
-  </main>
+    <button @click="addNotification()">Add new</button>
+</main>
 </template>
