5cdda2ce2c2859a65349b00897c453b22834c12a
jagger-en <japman2400@gmail.com>
2023-11-11 13:47:37 +0200

    Show data from firestore

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,48 @@
 <script setup>
 import Notification from './components/Notification.vue'
+
+import firebaseConfig from "../firebaseInit";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      notifications: []
+    }
+  },
+  mounted() {
+    this.getNotifications().then((n) => {
+      this.notifications = n
+    })
+  },
+  methods: {
+    async getNotifications() {
+      const app = initializeApp(firebaseConfig);
+      const fs = getFirestore(app)
+      const collection_ref = collection(fs, "notifications")
+
+      const notifications = []
+      await getDocs(collection_ref).then((snapshot) => {
+        snapshot.docs.forEach(d => {
+          notifications.push(d.data())
+        })
+      })
+
+      return notifications
+    }
+  }
+}
 </script>
 
 <template>
   <main>
     <h1>Hello World</h1>
+    <li v-for="notification in notifications">
+      {{ notification.msg }}
+    </li>
     <Notification msg="This is the notification." />
   </main>
 </template>
